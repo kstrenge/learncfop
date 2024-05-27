991602db9ef5538831d3ec957dd42f3290f04ab5
koncode <konrad.strenge@gmail.com>
2024-05-27 17:54:02 +1200

    added home page

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -6,14 +6,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Rubik App")),
+      appBar: AppBar(
+        title: const Text("Rubik App"),
+        centerTitle: true,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Center(
-          child: Text(
-            "WELCOME TO RUBIK, \n YOUR SPEEDCUBE MEMORY AID",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.displaySmall,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 64),
+              Text("WELCOME TO RUBIK, \n YOUR SPEEDCUBE MEMORY AID", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headlineLarge),
+              const SizedBox(height: 16),
+              ClipRect(
+                child: Align(
+                  heightFactor: 0.5,
+                  child: Image.asset(
+                    width: 300,
+                    "assets/icon/icon_foreground.png",
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              Text(
+                "Find the solving algorithms in the \n tabs at the bottom",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 16),
+              const Icon(Icons.arrow_downward, size: 30),
+              const Spacer(),
+              const Text("made by koncode"),
+              const SizedBox(height: 16),
+              TextButton.icon(
+                label: const Text("Open Source Licences"),
+                icon: const Icon(Icons.open_in_browser),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => const LicensePage()),
+                  );
+                },
+              ),
+              const SizedBox(height: 16),
+            ],
           ),
         ),
       ),
