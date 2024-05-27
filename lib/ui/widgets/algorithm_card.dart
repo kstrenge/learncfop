c885f8d49f752c40077f57779ca06d522e8223a0
koncode <konrad.strenge@gmail.com>
2024-05-27 16:44:44 +1200

    cleanup

--- lib/ui/widgets/algorithm_card.dart
+++ lib/ui/widgets/algorithm_card.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import '../../data/algorithm.dart';
 import 'oll_case_icon.dart';
