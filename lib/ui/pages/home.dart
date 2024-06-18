8921b2a340e74ce55d7aed32998fcb478d8ded59
k0ncode <konrad.strenge@gmail.com>
2024-06-18 02:28:57 +0200

    flexible layout for home page

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -10,46 +10,49 @@ class Home extends StatelessWidget {
         title: const Text("Rubik App"),
         centerTitle: true,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const SizedBox(height: 64),
-              Text("WELCOME TO RUBIK, \n YOUR SPEEDCUBE MEMORY AID", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headlineLarge),
-              const SizedBox(height: 16),
-              ClipRect(
-                child: Align(
-                  heightFactor: 0.5,
-                  child: Image.asset(
-                    width: 300,
-                    "assets/icon/icon_foreground.png",
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 32),
+                Text("WELCOME TO RUBIK, \n YOUR SPEEDCUBE MEMORY AID", textAlign: TextAlign.center, style: Theme.of(context).textTheme.headlineLarge),
+                const SizedBox(height: 16),
+                ClipRect(
+                  child: Align(
+                    heightFactor: 0.5,
+                    child: Image.asset(
+                      width: 300,
+                      "assets/icon/icon_foreground.png",
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 32),
-              Text(
-                "Find the solving algorithms in the \n tabs at the bottom",
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.bodyLarge,
-              ),
-              const SizedBox(height: 16),
-              const Icon(Icons.arrow_downward, size: 30),
-              const Spacer(),
-              const Text("made by koncode"),
-              const SizedBox(height: 16),
-              TextButton.icon(
-                label: const Text("Open Source Licences"),
-                icon: const Icon(Icons.open_in_browser),
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const LicensePage()),
-                  );
-                },
-              ),
-              const SizedBox(height: 16),
-            ],
+                const SizedBox(height: 32),
+                Text(
+                  "Find the solving algorithms in the \n tabs at the bottom",
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+                const SizedBox(height: 16),
+                const Icon(Icons.arrow_downward, size: 30),
+                // const Spacer(),
+                const SizedBox(height: 64),
+                const Text("made by koncode"),
+                const SizedBox(height: 16),
+                TextButton.icon(
+                  label: const Text("Open Source Licences"),
+                  icon: const Icon(Icons.open_in_browser),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const LicensePage()),
+                    );
+                  },
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
           ),
         ),
       ),
