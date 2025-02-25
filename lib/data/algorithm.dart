c70b810c48f1d6ad7b4f12ba72d4436bb97a0db5
k0ncode <konrad.strenge@gmail.com>
2025-02-25 03:31:54 +0100

    datastructure rewrite

--- lib/data/algorithm.dart
+++ lib/data/algorithm.dart
@@ -1,54 +1,53 @@
 abstract class Algorithm {
-  final String title;
-  final String algorithm;
-  final String? notes;
+  String id;
+  String label;
+  String algorithm;
+  String? notes;
+  bool isFavourite;
 
-  Algorithm({
-    required this.title,
-    required this.algorithm,
-    this.notes,
-  });
+  Algorithm.fromJson(Map<String, dynamic> json)
+      : id = json["id"],
+        label = json["label"],
+        algorithm = json["algorithm"],
+        notes = json["notes"],
+        isFavourite = json["isFavourite"] ?? false;
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "label": label,
+        "algorithm": algorithm,
+        "notes": notes,
+        "isFavourite": isFavourite,
+      };
 }
 
 class OLLAlgorithm extends Algorithm {
   final List<int> caseConfiguration;
 
-  OLLAlgorithm({
-    required super.title,
-    required super.algorithm,
-    super.notes,
-    required this.caseConfiguration,
-  });
+  OLLAlgorithm.fromJson(Map<String, dynamic> json)
+      : caseConfiguration = json["caseConfiguration"].cast<int>(),
+        super.fromJson(json);
 
-  factory OLLAlgorithm.fromJson(Map<String, dynamic> json) {
-    return OLLAlgorithm(
-      title: json["title"],
-      caseConfiguration: json["caseConfiguration"].cast<int>(),
-      algorithm: json["algorithm"],
-      notes: json["notes"],
-    );
-  }
+  @override
+  Map<String, dynamic> toJson() => {
+        ...super.toJson(),
+        "caseConfiguration": caseConfiguration,
+      };
 }
 
 class PLLAlgorithm extends Algorithm {
   final List<String> caseConfiguration;
-  final List<List> arrows;
+  final List<List<dynamic>> arrows;
 
-  PLLAlgorithm({
-    required super.title,
-    required this.caseConfiguration,
-    required this.arrows,
-    required super.algorithm,
-    super.notes,
-  });
+  PLLAlgorithm.fromJson(Map<String, dynamic> json)
+      : caseConfiguration = json["caseConfiguration"].cast<String>(),
+        arrows = json["arrows"].cast<List<dynamic>>(),
+        super.fromJson(json);
 
-  factory PLLAlgorithm.fromJson(Map<String, dynamic> json) {
-    return PLLAlgorithm(
-      title: json["title"],
-      caseConfiguration: json["caseConfiguration"].cast<String>(),
-      arrows: json["arrows"].cast<List>(),
-      algorithm: json["algorithm"],
-      notes: json["notes"],
-    );
-  }
+  @override
+  Map<String, dynamic> toJson() => {
+        ...super.toJson(),
+        "caseConfiguration": caseConfiguration,
+        "arrows": arrows,
+      };
 }
